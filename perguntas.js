3ea742dcb94b8d1d8f37cc22e5041eb22920434a
Alessandra0701 <endrice.alessandra@escola.pr.gov.br>
2025-10-17 15:51:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 2+2?',
+    '4'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é investimento?',
+    'É qualquer recurso que gere um lucro com o tempo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz AMOR em Inglês?',
+    'LOVE'
+)
